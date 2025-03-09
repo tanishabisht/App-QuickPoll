aa91ae3c1480151e171feb123bdca1ab39746c1b
Tanisha Bisht <agamyatani@gmail.com>
2025-03-08 20:55:31 -0500

    add poll component

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Header from './components/Header.svelte';
+	import PollList from './components/PollList.svelte';
+</script>
+
+<Header />
+<main>
+  <PollList />
+</main>
+
+<style>
+	main{
+    width: 100%;
+    max-width: 960px;
+    margin: 40px auto;
+  }
+</style>
---
+++ src/components/PollList.svelte
@@ -0,0 +1,23 @@
+<script>
+  import PollStore from '../stores/pollStore.js';
+</script>
+
+<div class="poll-list">
+  {#each $PollStore as poll (poll.id)}  
+    <div>
+      <h1>{poll.question}</h1>
+      <p>{poll.answerA}</p>
+      <p>{poll.answerB}</p>
+      <p>{poll.votesA}</p>
+      <p>{poll.votesB}</p>
+    </div>
+  {/each}
+</div>
+
+<style>
+  .poll-list{
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 20px;
+  }
+</style>
