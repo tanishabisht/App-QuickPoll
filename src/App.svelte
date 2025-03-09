bb0f3cf59d251575ebf346c39b541cc23a32969b
Tanisha Bisht <agamyatani@gmail.com>
2025-03-08 21:13:29 -0500

    update main page with two tabs

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,28 @@
 <script>
 	import Header from './components/Header.svelte';
 	import PollList from './components/PollList.svelte';
+	import CreatePollForm from './components/CreatePollForm.svelte';
+  import Tabs from './shared/Tabs.svelte';
+
+  // tabs
+  let items = ['Current Polls', 'Add New Poll'];
+  let activeItem = 'Current Polls';
+
+  const handleTabChange = (e) => activeItem = e.detail;
+
+  const handleAdd = () => {
+    activeItem = 'Current Polls';
+  }
 </script>
 
 <Header />
 <main>
-  <PollList />
+  <Tabs {activeItem} {items} on:tabChange={handleTabChange} />
+  {#if activeItem === 'Current Polls'}
+    <PollList />
+  {:else if activeItem === 'Add New Poll'}
+    <CreatePollForm on:add={handleAdd} />
+  {/if}
 </main>
 
 <style>
